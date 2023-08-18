208f0301c91531e7ffe02d0b76b75030b5efacec
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-18 15:06:02 +0300

    writes signup controller

---
+++ controllers/signUpController.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
+
+const signup = async (req, res) => {
+    const body = req.body;
+    if (Object.keys(body).length === 0){
+        res.status(400).json({"message": "Please provide registration data"})
+    } else {
+
+        const hashedPassword = await bcrypt.hash(body.password, 10)
+        const user = new User({
+            email: body.email,
+            password: hashedPassword,
+            firstName: body.firstName,
+            lastName: body.lastName,
+        });
+
+        user.save().then((value) => {
+            console.log(value);
+            res.status(200).json({"message": "User Created", "user": value,});
+        });
+    
+    }
+
+}
