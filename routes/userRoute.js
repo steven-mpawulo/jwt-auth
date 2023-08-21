6fb8b01b906d5ebe69034268c798f576c0377b9d
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-21 15:18:18 +0300

    adds getting single user to user router

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,11 +3,13 @@ const signup = require('../controllers/signUpController');
 const login = require('../controllers/loginController');
 const getAllUsersController = require('../controllers/getAllUsersController');
 const verifyToken = require('../middleware/verifyToken');
+const getSingleUserController = require('../controllers/getSingleUserController');
 const userRouter = express.Router();
 
 // signup user 
 userRouter.post('/signup', signup);
 userRouter.post('/login', login);
-userRouter.get('/users', verifyToken, getAllUsersController)
+userRouter.get('/users', verifyToken, getAllUsersController);
+userRouter.get('/users/:id', verifyToken, getSingleUserController);
 
 module.exports = userRouter;
