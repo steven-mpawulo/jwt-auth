a1732225ac9cc31654f6bd5088b4c6b4306f3a3d
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-21 15:07:18 +0300

    works on adding jwt authetication to get all users route

--- controllers/getAllUsersController.js
+++ controllers/getAllUsersController.js
@@ -2,10 +2,11 @@ const User = require("../models/User")
 
 const getAllUsersController = async (req, res) => {
     await User.find({}).then((value) => {
+        console.log(req.user);
         if (value){
             console.log(value);
             res.status(200).json({"users": value});
-        } {
+        } else {
             res.status(400).json({"message": "failed to retreive users"})
         }
 
--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,21 +1,22 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization.split('')[1];
+const verifyToken = async (req, res, next) => {
+    console.log("Am here");
+    const token = req.headers.authorization.split(" ")[1];
     console.log(token);
-
-    // if (token){
-    //     const tokenVerification = jwt.verify(token, process.env.SECRETKEY)
-    //     res.status(200).json({
-    //         "token verified": tokenVerification,
-    //     });
-    //     next();
-    // } else {
-    //     res.status(400).json({
-    //         "message": "failed to verify"
-    //     });
-    // }
+    const tokenVerification = await jwt.verify(token, process.env.SECRETKEY);
+    console.log(tokenVerification);
+    if (tokenVerification){
+        req.user = tokenVerification;
+    } else {
+        res.sendStatus(400);
+    }
+    
+    // res.status(200).json({
+    //     "token verified": tokenVerification,
+    // });
+    next();
 }
 
 module.exports = verifyToken;
