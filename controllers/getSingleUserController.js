503fac29eeb630cefc061b349ba7a46c07410ca8
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-21 15:41:54 +0300

    modifies verify token and get single user controller

--- controllers/getSingleUserController.js
+++ controllers/getSingleUserController.js
@@ -3,14 +3,19 @@ const User = require("../models/User");
 const getSingleUserController = (req, res) => {
 const user = req.user;
 console.log(user);
-User.findOne({"_id": user._id}).then((value) => {
-    console.log(value);
-    if (value){
-        res.status(200).json({"user": value});
-    } else {
-        res.status(400).json({"message": "something went wrong"});
-    }
-});
+if (user !== undefined) {
+    User.findOne({"_id": user._id}).then((value) => {
+        console.log(value);
+        if (value){
+            res.status(200).json({"user": value});
+        } else {
+            res.status(400).json({"message": "something went wrong"});
+        }
+    });
+} else {
+    res.status(400).json({"message": "something went wrong"})
+}
+
 }
 
 module.exports = getSingleUserController;
--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,21 +1,18 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const verifyToken = async (req, res, next) => {
+const verifyToken = (req, res, next) => {
     console.log("Am here");
     const token = req.headers.authorization.split(" ")[1];
     console.log(token);
-    const tokenVerification = await jwt.verify(token, process.env.SECRETKEY);
-    console.log(tokenVerification);
-    if (tokenVerification){
-        req.user = tokenVerification;
-    } else {
-        res.sendStatus(400);
-    }
-    
-    // res.status(200).json({
-    //     "token verified": tokenVerification,
-    // });
+    jwt.verify(token, process.env.SECRETKEY, (err, data) => {
+        if (err){
+            // res.sendStatus(400);
+        } else {
+            console.log(data);
+            req.user = data;
+        }
+    });
     next();
 }
 
