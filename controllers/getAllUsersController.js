78d3a1f98fd905a44290adc3232a3a9cacbf24d9
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-21 16:38:13 +0300

    adds error handling capabilities to routes

--- controllers/getAllUsersController.js
+++ controllers/getAllUsersController.js
@@ -1,16 +1,22 @@
 const User = require("../models/User")
 
 const getAllUsersController = async (req, res) => {
-    await User.find({}).then((value) => {
-        console.log(req.user);
-        if (value){
-            console.log(value);
-            res.status(200).json({"users": value});
-        } else {
-            res.status(400).json({"message": "failed to retreive users"})
-        }
-
-    });
+    const user = req.user;
+    if (user !== undefined) {
+        await User.find({}).then((value) => {
+            console.log(req.user);
+            if (value){
+                console.log(value);
+                res.status(200).json({"users": value});
+            } else {
+                res.status(400).json({"message": "failed to retreive users"})
+            }
+    
+        });
+    } else {
+        res.json({"message": "Something went wrong"});
+    }
+    
 }
 
 module.exports = getAllUsersController;
