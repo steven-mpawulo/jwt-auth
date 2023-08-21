967d93d21f61fca6bda3240747090f6c7833d07f
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-21 15:16:59 +0300

    adds fetching single user information

---
+++ controllers/getSingleUserController.js
@@ -0,0 +1,14 @@
+const User = require("../models/User");
+
+const getSingleUserController = (req, res) => {
+const user = req.user;
+console.log(user);
+User.findOne({"_id": user._id}).then((value) => {
+    console.log(value);
+    if (value){
+        res.status(200).json({"user": value});
+    } else {
+        res.status(400).json({"message": "something went wrong"});
+    }
+});
+}
