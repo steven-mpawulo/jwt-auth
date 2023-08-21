f6f9ba187e12a3dbeaa1babd4dc732229bcfece1
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-21 13:25:20 +0300

    corrects authorization error

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,20 +2,20 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization'].split('')[1]
+    const token = req.headers.authorization.split('')[1];
     console.log(token);
 
-    if (token){
-        const tokenVerification = jwt.verify(token, process.env.SECRETKEY)
-        res.status(200).json({
-            "token verified": tokenVerification,
-        });
-        next();
-    } else {
-        res.status(400).json({
-            "message": "failed to verify"
-        });
-    }
+    // if (token){
+    //     const tokenVerification = jwt.verify(token, process.env.SECRETKEY)
+    //     res.status(200).json({
+    //         "token verified": tokenVerification,
+    //     });
+    //     next();
+    // } else {
+    //     res.status(400).json({
+    //         "message": "failed to verify"
+    //     });
+    // }
 }
 
 module.exports = verifyToken;
