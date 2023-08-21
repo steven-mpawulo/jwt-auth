fbe092770fe9df8d576abe8bfddebf6e26bfa933
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-21 09:54:47 +0300

    works on get all users controller

---
+++ controllers/getAllUsersController.js
@@ -0,0 +1,13 @@
+const User = require("../models/User")
+
+const getAllUsersController = async (req, res) => {
+    await User.find({}).then((value) => {
+        if (value){
+            console.log(value);
+            res.status(200).json({"users": value});
+        } {
+            res.status(400).json({"message": "failed to retreive users"})
+        }
+
+    });
+}
